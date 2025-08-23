c00fabfa67ee391464b3de80350d218ae523397b
spoutOG <orange.tiger617@gmail.com>
2025-08-22 21:57:23 -0400

    Pre Release 2, Dark Mode  and design changes

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
 function myFunction() {
    var element = document.body;
    element.classList.toggle("dark-mode");
+   if (element.classList.contains("dark-mode")) {
+      localStorage.setItem("darkMode", "enabled");
+   } else {
+      localStorage.setItem("darkMode", "disabled");
+   }
 }
+
+// On page load, set dark mode if remembered
+window.addEventListener("DOMContentLoaded", function() {
+   if (localStorage.getItem("darkMode") === "enabled") {
+      document.body.classList.add("dark-mode");
+   }
+});
